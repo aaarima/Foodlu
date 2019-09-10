fa0c790545f0232a21908d4627bcbd9882592616
Aaron Arima <aaarima@yahoo.com>
2019-09-10 16:55:38 -0700

    commented out active storage associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
   validates :description, :content_rating, :year, presence: true
   validates :title, presence: true, uniqueness: true
-
-  has_one_attached :movie
-  has_one_attached :thumbnail
+  #
+  # has_one_attached :movie
+  # has_one_attached :thumbnail
 end
