716eb2cd42a6c5b9171f16bec73609b63e08e205
Aaron Arima <aaarima@yahoo.com>
2019-09-12 16:11:11 -0700

    added bundle file correctly to git ignore

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -6,6 +6,11 @@ import { Link } from "react-router-dom";
 const Home = props => (
   <div>
     <NavBar left={[<Link to={"/"}><p key={"logo"} className="logo">foodlu</p></Link>]} right={[<UserDropdownContainer/>]} />
+    <div className="welcome-image-container">
+      <div className="gradient"></div>
+      <h1>Placeholder stuff for now</h1>
+      <img src="https://chopra.com/sites/default/files/field/image/6dailyhabitsofhappypeople2.jpg" alt=""/>
+    </div>
   </div>
 );
 
